5456fbab6a8357165d3d7ed68f97fb68cd510ffd
kimtree122 <839414748@qq.com>
2019-10-31 11:37:37 +0800

    添加下载使用数据流功能

--- KNCore.API/Areas/FileUpDownLoad/Controllers/FileController.cs
+++ KNCore.API/Areas/FileUpDownLoad/Controllers/FileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using KNCore.Comm;
 using KNCore.Comm.FileHelper;
 using Microsoft.AspNetCore.Http;
@@ -46,7 +47,7 @@ namespace KNCore.API.Areas.FileUpDownLoad.Controllers
             return Json(fn);
         }
 
-        [HttpPost("LoadDownFile")]
+        [HttpGet("LoadDownFile")]
         public async Task<IActionResult> LoadDownFile(string filePath)
         {
             var memory = new MemoryStream();
@@ -58,6 +59,57 @@ namespace KNCore.API.Areas.FileUpDownLoad.Controllers
             var file = File(memory, GetContentType(filePath), Path.GetFileName(filePath));
             return file;
         }
+
+        //文件流下载
+        [HttpGet("DownLoadFileInStream")]
+        public IActionResult DownLoadFileInStream(string filePath)
+        {
+            //var memory = new MemoryStream();
+            var fileName = Path.GetFileName(filePath);//测试文档.xlsx
+
+            int bufferSize = 1;//这就是ASP.NET Core循环读取下载文件的缓存大小，这里我们设置为了1024字节，也就是说ASP.NET Core每次会从下载文件中读取1024字节的内容到服务器内存中，然后发送到客户端浏览器，这样避免了一次将整个下载文件都加载到服务器内存中，导致服务器崩溃
+
+            Response.ContentType = "application/octet-stream";//由于我们下载的是一个Excel文件，所以设置ContentType为application/vnd.ms-excel
+
+            var contentDisposition = "attachment;" + "filename=" + HttpUtility.UrlEncode(fileName);//在Response的Header中设置下载文件的文件名，这样客户端浏览器才能正确显示下载的文件名，注意这里要用HttpUtility.UrlEncode编码文件名，否则有些浏览器可能会显示乱码文件名
+            Response.Headers.Add("Content-Disposition", new string[] { contentDisposition });
+
+            //使用FileStream开始循环读取要下载文件的内容
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (Response.Body)//调用Response.Body.Dispose()并不会关闭客户端浏览器到ASP.NET Core服务器的连接，之后还可以继续往Response.Body中写入数据
+                {
+                    long contentLength = fs.Length;//获取下载文件的大小
+                    Response.ContentLength = contentLength;//在Response的Header中设置下载文件的大小，这样客户端浏览器才能正确显示下载的进度
+
+                    byte[] buffer;
+                    long hasRead = 0;//变量hasRead用于记录已经发送了多少字节的数据到客户端浏览器
+
+                    //如果hasRead小于contentLength，说明下载文件还没读取完毕，继续循环读取下载文件的内容，并发送到客户端浏览器
+                    while (hasRead < contentLength)
+                    {
+                        //HttpContext.RequestAborted.IsCancellationRequested可用于检测客户端浏览器和ASP.NET Core服务器之间的连接状态，如果HttpContext.RequestAborted.IsCancellationRequested返回true，说明客户端浏览器中断了连接
+                        if (HttpContext.RequestAborted.IsCancellationRequested)
+                        {
+                            //如果客户端浏览器中断了到ASP.NET Core服务器的连接，这里应该立刻break，取消下载文件的读取和发送，避免服务器耗费资源
+                            break;
+                        }
+
+                        buffer = new byte[bufferSize];
+
+                        int currentRead = fs.Read(buffer, 0, bufferSize);//从下载文件中读取bufferSize(1024字节)大小的内容到服务器内存中
+
+                        Response.Body.Write(buffer, 0, currentRead);//发送读取的内容数据到客户端浏览器
+                        Response.Body.Flush();//注意每次Write后，要及时调用Flush方法，及时释放服务器内存空间
+
+                        hasRead += currentRead;//更新已经发送到客户端浏览器的字节数
+                    }
+                }
+            }
+
+            return new EmptyResult();
+        }
+
 
         public async Task<IActionResult> Findimage(int width, string name)
         {
@@ -149,7 +201,7 @@ namespace KNCore.API.Areas.FileUpDownLoad.Controllers
 
         private Dictionary<string, string> GetMimeTypes()
         {
-            return new Dictionary<string, string> { { ".txt", "text/plain" }, { ".pdf", "application/pdf" }, { ".doc", "application/vnd.ms-word" }, { ".docx", "application/vnd.ms-word" }, { ".xls", "application/vnd.ms-excel" }, { ".xlsx", "application/vnd.openxmlformats officedocument.spreadsheetml.sheet" }, { ".png", "image/png" }, { ".jpg", "image/jpeg" }, { ".jpeg", "image/jpeg" }, { ".gif", "image/gif" }, { ".csv", "text/csv" } };
+            return new Dictionary<string, string> { { ".txt", "text/plain" }, { ".pdf", "application/pdf" }, { ".doc", "application/vnd.ms-word" }, { ".docx", "application/vnd.ms-word" }, { ".xls", "application/vnd.ms-excel" }, { ".xlsx", "application/vnd.openxmlformats officedocument.spreadsheetml.sheet" }, { ".png", "image/png" }, { ".jpg", "image/jpeg" }, { ".jpeg", "image/jpeg" }, { ".gif", "image/gif" }, { ".csv", "text/csv" },{".exe", "application/octet-stream" } };
         }
 
 
